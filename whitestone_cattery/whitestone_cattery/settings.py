b4646929b3c7dd1dd63a6f98cde6cfa84efee523
Safi Hasan <safihasan@gmail.com>
2024-07-08 12:32:07 +0000

    Correct runtime

--- whitestone_cattery/whitestone_cattery/settings.py
+++ whitestone_cattery/whitestone_cattery/settings.py
@@ -63,9 +63,10 @@ WSGI_APPLICATION = 'whitestone_cattery.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
